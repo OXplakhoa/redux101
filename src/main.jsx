e571f872b27d02b0c160f5b299f38851438c2b93
OXplakhoa <OXplakhoa@gmail.com>
2024-10-26 16:40:38 +0700

    Add redux function for the 1st time!

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,17 @@ const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModify = (count = 0) => {
-  count++;
-  return count;
+const countModify = (count = 0, action) => {
+  if(action.type === "add"){
+    return count + 1;
+  }else if (action.type === "minus"){
+    return count - 1;
+  }else {
+    return count;
+  }
 }
+
+number.innerText = 0;
 
 const countStore = configureStore({
   reducer: {
@@ -15,4 +22,19 @@ const countStore = configureStore({
   }
 })
 
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState().count;
+}
+ 
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type: "add"});
+}
+//dispatch send messages to countModify
+const handleMinus = () => {
+  countStore.dispatch({type: "minus"});
+}
+
+plus.addEventListener("click",handleAdd) 
+minus.addEventListener("click",handleMinus)
