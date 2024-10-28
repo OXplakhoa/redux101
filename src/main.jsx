658d12d416dae29169aae10e1f5cf9f371c0d650
OXplakhoa <OXplakhoa@gmail.com>
2024-10-28 17:55:18 +0700

    Refactor Variable

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,42 @@
-import {configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 
 const plus = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const neg = document.getElementById("minus");
+const span = document.querySelector("span");
+span.innerText = 0;
 
-const countModify = (count = 0, action) => {
-  if(action.type === "add"){
-    return count + 1;
-  }else if (action.type === "minus"){
-    return count - 1;
-  }else {
-    return count;
+const add = "add";
+const minus = "minus";
+
+const handleCount = (count = 0,action) => {
+  switch (action.type) {
+    case add:
+      return count+1;
+    case minus:
+      return count-1;
+    default:
+      return count;
   }
 }
 
-number.innerText = 0;
-
 const countStore = configureStore({
   reducer: {
-    count: countModify
+    c: handleCount
   }
 })
 
 const onChange = () => {
-  number.innerText = countStore.getState().count;
+  span.innerText = countStore.getState().c;
 }
- 
-countStore.subscribe(onChange);
+
+countStore.subscribe(onChange)
 
 const handleAdd = () => {
-  countStore.dispatch({type: "add"});
+  countStore.dispatch({type: add})
 }
-//dispatch send messages to countModify
 const handleMinus = () => {
-  countStore.dispatch({type: "minus"});
+  countStore.dispatch({type: minus})
 }
 
-plus.addEventListener("click",handleAdd) 
-minus.addEventListener("click",handleMinus)
+plus.addEventListener("click",handleAdd);
+neg.addEventListener("click",handleMinus);
