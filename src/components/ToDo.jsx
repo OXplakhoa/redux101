37c21852bdb6989b38e62f023e3c480a73a6f700
OXplakhoa <OXplakhoa@gmail.com>
2024-10-30 19:37:58 +0700

    Practice mapStateToProps & Add Details

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-function ToDo({text,onBtnClick}){ //Need to destructure the props{} so that React know which key belongs to which value
+function ToDo({text,onBtnClick,id}){ //Need to destructure the props{} so that React know which key belongs to which value
     return (
         <div>
-            <li>
-                {text} <button onClick={onBtnClick}>X</button> 
-            </li>
+            <Link to={`/${id}`}>
+                <li>
+                    {text} <button onClick={onBtnClick}>X</button> 
+                </li>
+            </Link>
         </div>
     )
 }
--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,7 +1,21 @@
-export default function Detail(){
+import React from "react";
+import { connect } from "react-redux";
+
+function Detail({toDos}) {
     return (
         <div>
-            DETAIL
+            <h1>Details: </h1>
+            {toDos.map((toDo) => (
+                <ul key={toDo.id}>
+                    <li >{toDo.text}</li>
+                </ul>
+            ))}
         </div>
     )
 }
+
+const mapStateToProps = (state,ownProps) => {
+    return {toDos: state}
+}
+
+export default connect (mapStateToProps) (Detail)
