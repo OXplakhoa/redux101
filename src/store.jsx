20c7253b4b05fd959ddfcafef370993e8d4f5ebf
OXplakhoa <OXplakhoa@gmail.com>
2024-11-04 22:00:00 +0700

    Add createReducer

--- src/store.jsx
+++ src/store.jsx
@@ -1,21 +1,30 @@
-import { configureStore, createAction } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
 const addToDo = createAction("ADD")
 const deleteToDo = createAction("DELETE")
 
-
+/*
 const handleReducer = (state = [], action) => {
     switch (action.type) {
         case addToDo.type:
             return [{ text: action.payload, id: uuidv4() }, ...state];
-        case deleteToDo.type:
-            console.log(action);
-            return state.filter((s) => s.id !== action.payload);
-        default:
-            return state;
-    }
-};
+            case deleteToDo.type:
+                console.log(action); 
+                return state.filter((s) => s.id !== action.payload);
+                default:
+                    return state;
+                }
+            };
+*/
+
+const handleReducer = createReducer([],(builder) => {
+    builder
+        .addCase(addToDo,(state,action) => {
+            state.push({text: action.payload,id: uuidv4()})
+        })
+        .addCase(deleteToDo,(state,action) => state.filter((s) => s.id !== action.payload))
+})
 
 export const store = configureStore({
     reducer: handleReducer
