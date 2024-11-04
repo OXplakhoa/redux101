21da946c5e2a15396175c31012a1d60a51ad718b
OXplakhoa <OXplakhoa@gmail.com>
2024-11-04 21:31:07 +0700

    Add createAction

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -6,11 +6,12 @@ import { Link } from "react-router-dom";
 function ToDo({text,onBtnClick,id}){ //Need to destructure the props{} so that React know which key belongs to which value
     return (
         <div>
-            <Link to={`/${id}`}>
-                <li>
-                    {text} <button onClick={onBtnClick}>X</button> 
-                </li>
-            </Link>
+            <li>
+                <Link to={`/${id}`}>
+                    {text} 
+                </Link>
+                <button onClick={onBtnClick}>X</button> 
+            </li>
         </div>
     )
 }
--- src/store.jsx
+++ src/store.jsx
@@ -1,29 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+const addToDo = createAction("ADD")
+const deleteToDo = createAction("DELETE")
 
-const addToDo = (text) => {
-    return {
-        type: ADD,
-        text
-    };
-};
-
-const deleteToDo = (id) => {
-    return {
-        type: DELETE,
-        id
-    };
-};
 
 const handleReducer = (state = [], action) => {
     switch (action.type) {
-        case ADD:
-            return [{ text: action.text, id: uuidv4() }, ...state];
-        case DELETE:
-            return state.filter((s) => s.id !== action.id);
+        case addToDo.type:
+            return [{ text: action.payload, id: uuidv4() }, ...state];
+        case deleteToDo.type:
+            console.log(action);
+            return state.filter((s) => s.id !== action.payload);
         default:
             return state;
     }
